8c02546c1fad15e38aca162539b388afc2d9ff99
mkulikov <mkulikov@student.42berlin.de>
2025-05-13 15:32:41 +0200

    test(ex00) add description before tests , remove spaces

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 17:14:37 by mkulikov          #+#    #+#             */
-/*   Updated: 2025/04/22 17:16:04 by mkulikov         ###   ########.fr       */
+/*   Updated: 2025/05/13 15:30:44 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ int main( void )
 {
 	int a = 2;
 	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+	std::cout << "Before swap:" << std::endl << "a = " << a << ", b = " << b << std::endl;
+	::swap(a, b);
+	std::cout << "After swap:" << std::endl << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+	std::string c = "world1";
+	std::string d = "world2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
 	return 0;
 }
