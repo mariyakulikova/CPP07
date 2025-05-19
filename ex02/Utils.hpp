28ee0b56016e2301c165734945d562220b136596
Mariia Kulikova <mkulikov@student.42berlin.de>
2025-05-19 18:59:49 +0200

    feat(ex02) add template method for printing array

---
+++ ex02/Utils.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 18:38:23 by mkulikov          #+#    #+#             */
+/*   Updated: 2025/05/19 18:38:40 by mkulikov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstddef>
+#include <iostream>
+
+template <typename T>
+void print_words(const T* words, std::size_t n)
+{
+	std::cout << "[";
+	for (std::size_t i = 0; i < n; ++i)
+	{
+		std::cout << words[i];
+		if (i < n - 1)
+			std::cout << " ";
+	}
+	std::cout << "]" << std::endl;
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,25 +6,13 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 13:00:59 by mkulikov          #+#    #+#             */
-/*   Updated: 2025/05/19 18:27:24 by mkulikov         ###   ########.fr       */
+/*   Updated: 2025/05/19 18:55:37 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
+// #include <string>
 #include "Array.hpp"
-
-void print_words(std::string* words, size_t n)
-{
-	std::cout << "[";
-	for (size_t i = 0; i < n; i++)
-	{
-		std::cout << words[i];
-		if (i < n - 1)
-			std::cout << " ";
-	}
-	std::cout << "]";
-	std::cout << std::endl;
-}
+#include "Utils.hpp"
 
 int main()
 {
@@ -42,6 +30,8 @@ int main()
 	std::cout << "Print words: " << words << std::endl;
 	std::cout << "Print words_copy: ";
 	print_words(words_copy, 5);
+	Array<std::string> words2(words);
+	std::cout << "Print words2: " << words2 << std::endl;
 	delete [] words_copy;
 	return 0;
 }
